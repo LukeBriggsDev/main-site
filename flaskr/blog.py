90e148ae3a807c441aa94d905ae27a05b7fd14ad
Luke Briggs <l.briggs3@newcastle.ac.uk>
2021-01-24 14:49:43 +0000

    Fix crash due to lack of argument

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -88,7 +88,7 @@ def update(id):
             db.commit()
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/update.html')
+    return render_template('blog/update.html', post=post)
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
