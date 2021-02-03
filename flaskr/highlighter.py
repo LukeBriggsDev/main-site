2c6a811c6574208f0a2a2cfa6d21c2f7c0b1a699
Luke Briggs <l.briggs3@newcastle.ac.uk>
2021-02-03 18:51:46 +0000

    Fix crash when unknown language is passed to highlighter

--- flaskr/highlighter.py
+++ flaskr/highlighter.py
@@ -7,9 +7,12 @@ from pygments.formatters import html
 class HighlightRenderer(mistune.Renderer):
     def block_code(self, code, lang=None):
         if lang:
-            lexer = get_lexer_by_name(lang, stripall=True)
-            formatter = html.HtmlFormatter(style='monokai', noclasses=True)
-            return highlight(code, lexer, formatter)
+            try:
+                lexer = get_lexer_by_name(lang, stripall=True)
+                formatter = html.HtmlFormatter(style='monokai', noclasses=True)
+                return highlight(code, lexer, formatter)
+            except ValueError:
+                pass
         return '<pre><code>' + mistune.escape(code) + '</code></pre>'
 
 
